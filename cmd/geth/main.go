d60e04a21ee3753b26f7393c8567095a62a39c2c
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2018-12-15 20:40:21 +0300

    add extdb command line paramter

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -39,6 +39,7 @@ import (
 	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/ethereum/go-ethereum/node"
 	cli "gopkg.in/urfave/cli.v1"
+	"github.com/ethereum/go-ethereum/extdb"
 )
 
 const (
@@ -136,6 +137,7 @@ var (
 		utils.EWASMInterpreterFlag,
 		utils.EVMInterpreterFlag,
 		configFileFlag,
+		extdb.ExtDbUriFlag,
 	}
 
 	rpcFlags = []cli.Flag{
@@ -278,6 +280,7 @@ func startNode(ctx *cli.Context, stack *node.Node) {
 	debug.Memsize.Add("node", stack)
 
 	// Start up the node itself
+	extdb.NewExtDBpg(ctx.GlobalString("extdb"))
 	utils.StartNode(stack)
 
 	// Unlock any account specifically requested
