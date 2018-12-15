384538b68dd0667bd1f7daa28635d344200d6d34
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2018-12-15 11:51:46 +0300

    add extdb package

---
+++ extdb/migrations/drop_tables.sql
@@ -0,0 +1,9 @@
+DROP TABLE headers;
+DROP TABLE bodies;
+DROP TABLE receipts;
+DROP TABLE accounts;
+DROP TABLE rewards;
+DROP TABLE internal_transactions;
+DROP TABLE pending_transactions;
+DROP TABLE chain_splits;
+DROP TABLE reorgs;
