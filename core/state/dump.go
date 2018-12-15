6c0724b2c3f340979ffc1a3a029a99e727c2c283
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2018-12-15 21:22:46 +0300

    store state object into extdb

--- core/state/dump.go
+++ core/state/dump.go
@@ -79,3 +79,25 @@ func (self *StateDB) Dump() []byte {
 
 	return json
 }
+
+func (self *StateDB) RawDumpStateObject(obj *stateObject) (DumpAccount, error) {
+	account := DumpAccount{
+		Balance:  obj.data.Balance.String(),
+		Nonce:    obj.data.Nonce,
+		Root:     common.Bytes2Hex(obj.data.Root[:]),
+		CodeHash: common.Bytes2Hex(obj.data.CodeHash),
+		Code:     common.Bytes2Hex(obj.Code(self.db)),
+		Storage:  nil,
+	}
+	// storageIt := trie.NewIterator(obj.getTrie(self.db).NodeIterator(nil))
+	// for storageIt.Next() {
+	// 	account.Storage[common.Bytes2Hex(self.trie.GetKey(storageIt.Key))] = common.Bytes2Hex(storageIt.Value)
+	// }
+	// json, err := json.MarshalIndent(account, "", "    ")
+	// if err != nil {
+	// 	fmt.Println("obj dump err", err)
+	// 	return nil, err
+	// }
+
+	return account, nil
+}
