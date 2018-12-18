e5e7fa52e6dc89079398363f36d3ebe16a407c48
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2018-12-18 17:35:03 +0300

    JSEARCH-94 cummulative db time logging

--- core/blockchain.go
+++ core/blockchain.go
@@ -1130,6 +1130,7 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals bool) (int, []
 		lastCanon     *types.Block
 		coalescedLogs []*types.Log
 	)
+	extdb.ResetDbWriteDuration()
 	// Start the parallel header verifier
 	headers := make([]*types.Header, len(chain))
 	seals := make([]bool, len(chain))
@@ -1286,6 +1287,7 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals bool) (int, []
 	if lastCanon != nil && bc.CurrentBlock().Hash() == lastCanon.Hash() {
 		events = append(events, ChainHeadEvent{lastCanon})
 	}
+	extdb.ResetDbWriteDuration()
 	return it.index, events, coalescedLogs, err
 }
 
