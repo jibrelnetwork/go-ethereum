aee43685db31bb3a9f9046c19fbbfe8be11a6e7c
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2019-01-30 08:57:01 +0300

    JSEARCH-94 fix cummulative extdb time logging

--- core/blockchain.go
+++ core/blockchain.go
@@ -1733,7 +1733,6 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals bool) (int, []
 	if lastCanon != nil && bc.CurrentBlock().Hash() == lastCanon.Hash() {
 		events = append(events, ChainHeadEvent{lastCanon})
 	}
-	extdb.ResetDbWriteDuration()
 	return it.index, events, coalescedLogs, err
 }
 
