f0d1be51446cfae70d2156d972bce58a1a1bcb6c
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2019-02-20 20:09:51 +0300

    chain_splits table linked with reorgs table

--- core/blockchain.go
+++ core/blockchain.go
@@ -1252,8 +1252,7 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals bool) (int, []
 				"diff", block.Difficulty(), "elapsed", common.PrettyDuration(time.Since(start)),
 				"txs", len(block.Transactions()), "gas", block.GasUsed(), "uncles", len(block.Uncles()),
 				"root", block.Root())
-			
-			extdb.WriteReorg(block.Hash(), block.Number().Uint64(), block.Header())
+			extdb.WriteReorg(nil, 0, block.Hash(), block.Number().Uint64(), block.Header())
 			events = append(events, ChainSideEvent{block})
 		}
 		blockInsertTimer.UpdateSince(start)
@@ -1470,6 +1469,10 @@ func (bc *BlockChain) reorg(oldBlock, newBlock *types.Block) error {
 			return fmt.Errorf("Invalid new chain")
 		}
 	}
+
+	tx, err := extdb.BeginTx()
+	var chain_split_id int
+
 	// Ensure the user sees large reorgs
 	if len(oldChain) > 0 && len(newChain) > 0 {
 		logFn := log.Debug
@@ -1478,7 +1481,7 @@ func (bc *BlockChain) reorg(oldBlock, newBlock *types.Block) error {
 		}
 		logFn("Chain split detected", "number", commonBlock.Number(), "hash", commonBlock.Hash(),
 			"drop", len(oldChain), "dropfrom", oldChain[0].Hash(), "add", len(newChain), "addfrom", newChain[0].Hash())
-		extdb.WriteChainSplit(commonBlock.NumberU64(), commonBlock.Hash(), len(oldChain), oldChain[0].Hash(), len(newChain), newChain[0].Hash())
+		chain_split_id, err = extdb.WriteChainSplit(tx, commonBlock.NumberU64(), commonBlock.Hash(), len(oldChain), oldChain[0].Hash(), len(newChain), newChain[0].Hash())
 	} else {
 		log.Error("Impossible reorg, please file an issue", "oldnum", oldBlock.Number(), "oldhash", oldBlock.Hash(), "newnum", newBlock.Number(), "newhash", newBlock.Hash())
 	}
@@ -1487,11 +1490,12 @@ func (bc *BlockChain) reorg(oldBlock, newBlock *types.Block) error {
 	for i := len(newChain) - 1; i >= 0; i-- {
 		// insert the block in the canonical way, re-writing history
 		bc.insert(newChain[i])
-		extdb.ReinsertBlock(newChain[i].Hash(), newChain[i].NumberU64(), newChain[i].Header())
+		extdb.ReinsertBlock(tx, chain_split_id, newChain[i].Hash(), newChain[i].NumberU64(), newChain[i].Header())
 		// write lookup entries for hash based transaction/receipt searches
 		rawdb.WriteTxLookupEntries(bc.db, newChain[i])
 		addedTxs = append(addedTxs, newChain[i].Transactions()...)
 	}
+
 	// calculate the difference between deleted and added transactions
 	diff := types.TxDifference(deletedTxs, addedTxs)
 	// When transactions get deleted from the database that means the
@@ -1506,13 +1510,20 @@ func (bc *BlockChain) reorg(oldBlock, newBlock *types.Block) error {
 		go bc.rmLogsFeed.Send(RemovedLogsEvent{deletedLogs})
 	}
 	if len(oldChain) > 0 {
+		for _, block := range oldChain {
+			extdb.WriteReorg(tx, chain_split_id, block.Hash(), block.Number().Uint64(), block.Header())
+		}
 		go func() {
 			for _, block := range oldChain {
-				extdb.WriteReorg(block.Hash(), block.Number().Uint64(), block.Header())
-				
 				bc.chainSideFeed.Send(ChainSideEvent{Block: block})
 			}
 		}()
+	}
+
+	if err != nil {
+		extdb.CloseTx(tx, false)
+	} else {
+		extdb.CloseTx(tx, true)
 	}
 
 	return nil
