336262b143440a7d0776aa132895f87abd513363
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2019-09-27 08:09:40 +0300

    rm migration

--- extdb/schema_migrations/20190925165720_drop_constraint_internal_txs.sql
+++
@@ -1,6 +0,0 @@
--- +goose Up
--- SQL in this section is executed when the migration is applied.
-DROP INDEX IF EXISTS internal_transactions_uniq;
-
--- +goose Down
--- SQL in this section is executed when the migration is rolled back.
